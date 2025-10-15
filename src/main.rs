e2b3d9ca9daed78147e21cd9b3f75b72a63f8c09
Rye <lepyrogang@proton.me>
2025-10-16 01:19:47 +0330

    fix: scrape logic implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use scraper::Selector;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = reqwest::Client::new();
+    let a = init_page_scrape("https://downloads.khinsider.com/game-soundtracks/album/risk-of-rain-2-survivors-of-the-void-2022", &client).await?;
+    let b = down_page_scrape(a, &client).await?;
+    println!("{:#?}", b);
+    Ok(())
+}
+
+async fn init_page_scrape(link: &'static str, client: &reqwest::Client) -> Result<Vec<String>> {
+    let mut a: Vec<String> = Vec::new();
+    let res: reqwest::Response = client.get(link).send().await.unwrap();
+    println!("Status: {}", res.status());
+    let html: String = res.text().await?;
+    println!("Parsing...");
+    let parsed: scraper::Html = scraper::Html::parse_document(&html);
+    let selector: Selector = Selector::parse("td.playlistDownloadSong a[href]").unwrap();
+    for element in parsed.select(&selector) {
+        a.push(
+            "https://downloads.khinsider.com/".to_string()
+                + &element.value().attr("href").unwrap().to_string(),
+        );
+    }
+
+    Ok(a)
+}
+async fn down_page_scrape(downlist: Vec<String>, client: &reqwest::Client) -> Result<Vec<String>> {
+    let mut a: Vec<String> = Vec::new();
+
+    for link in downlist {
+        let res = client.get(link).send().await?;
+        let html: String = res.text().await?;
+        let parsed: scraper::Html = scraper::Html::parse_document(&html);
+        let selector: Selector = Selector::parse("a[href]").unwrap();
+        for element in parsed.select(&selector) {
+            if let Some(href) = element.value().attr("href") {
+                if href.ends_with(".flac") {
+                    a.push(href.to_string());
+                }
+            }
+        }
+    }
+    Ok(a)
 }
